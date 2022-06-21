0aa70539c1a4091d6c796d72561fb100b14bb1d3
Laura Voss <lauravoss@posteo.de>
2022-06-21 21:46:54 +0100

    adding printStatement method

--- bank-account.js
+++ bank-account.js
@@ -17,6 +17,18 @@ class BankAccount {
     const subtotal = this.balance -= amount;
     this.transactions.push({credit: "", debit: amount, balance: subtotal});
   }
+
+  printStatement() {
+    const output = [];
+    const printOut = ["credit || debit || balance"];
+    console.log("transaction", this.transactions)
+    for(let i = 0; i < this.transactions.length; i ++){
+      output.unshift(`\n${this.transactions[i].credit} || ${this.transactions[i].debit} || ${this.transactions[i].balance}`);
+    }
+    printOut.push(output);
+    const str = printOut.flat(1).join('');
+    return str;
+  }
 }
   
 module.exports = BankAccount;
--- bank-account.test.js
+++ bank-account.test.js
@@ -1,11 +1,9 @@
 const BankAccount = require('./bank-account')
 
 describe('BankAccount', () => {
-  describe('.printStatement', () => {
-    it('should be created with an initial balance of 0', () => {
-      const account = new BankAccount();
-      expect(account.checkBalance()).toBe(0);
-    });
+  it('should be created with an initial balance of 0', () => {
+    const account = new BankAccount();
+    expect(account.checkBalance()).toBe(0);
   });
 
   describe('.deposit', () => {
@@ -38,7 +36,15 @@ describe('BankAccount', () => {
       account.withdraw(50);
       expect(account.transactions).toContainEqual({"credit": 100, "debit": "", "balance": 100}, {"credit": "", "debit": 50, "balance": 50});
     });
+  });  
+  
+  describe('printStatement', () => {
+    it('should return a string with transaction information for each transaction', () => {
+      const account = new BankAccount();
+      account.deposit(100);
+      account.withdraw(50);
+      expect(account.printStatement()).toEqual("credit || debit || balance\n || 50 || 50\n100 ||  || 100");
+    });
   });
-
 });
  
